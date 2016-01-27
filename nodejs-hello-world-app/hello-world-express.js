51a2755a5b3a21fa5d422fca9a7df65bbb629c1e
raduflorescu <radu.florescu@hotmail.com>
2016-01-27 13:29:44 +0000

    Made code more readable

--- nodejs-hello-world-app/hello-world-express.js
+++ nodejs-hello-world-app/hello-world-express.js
@@ -8,8 +8,8 @@ const PORT = 8080;
 // App
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello world</h1>');
+app.get('/', function (request, response) {
+  response.send('<h1>Hello world</h1>');
 });
 
 app.listen(PORT);
